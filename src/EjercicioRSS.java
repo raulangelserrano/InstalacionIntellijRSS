5f125ec44e525f5dc322e36c7801872aa84b7196
raulserrano <raulangel.serrano@gmail.com>
2018-05-12 11:20:20 +0200

    Cambios en rama privado realizados: Añadidos comentarios javadoc

--- src/EjercicioRSS.java
+++ src/EjercicioRSS.java
@@ -12,7 +12,16 @@ public class EjercicioRSS {
 
     }
 
-    static String veredicto(String valor1, String valor2) {
+    /**
+     *
+     * @param valor1 Indica la calificación que el alumno desea conseguir en el módulo
+     * @param valor2 Indica la calificación que realmente has conseguido.
+     * @return El método devuelve un String en función de los valores introducidos:
+     *     Si valor1==valor2 devuelve "Perfecto. Has tenido buen criterio."
+     *     Si valor1 < valor2 devuelve "Has mejorado, o te ha llegado el éxito inesperado."
+     *     Si valor1 > valor2 devuelve "Te has confiado. Falta realismo.";
+     */
+    private static String veredicto(String valor1, String valor2) {
         if (valor1.equals(valor2)) {
             return "Perfecto. Has tenido buen criterio.";
         }
